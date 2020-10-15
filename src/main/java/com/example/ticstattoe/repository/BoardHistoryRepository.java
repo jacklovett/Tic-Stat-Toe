90f45e687ab8716ffe6677d2c18626d3911a134f
Jack Lovett <jack.l.lovett@gmail.com>
2020-10-16 00:39:17 +0300

    Remove unnecessary repo functions

--- src/main/java/com/example/ticstattoe/repository/BoardHistoryRepository.java
+++ src/main/java/com/example/ticstattoe/repository/BoardHistoryRepository.java
@@ -1,7 +1,5 @@
 package com.example.ticstattoe.repository;
 
-import java.util.Optional;
-
 import com.example.ticstattoe.model.BoardHistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardHistoryRepository extends JpaRepository<BoardHistory, Long> {
 
-  Optional<BoardHistory> findByGameId(Long gameId);
 }
--- src/main/java/com/example/ticstattoe/repository/GameRepository.java
+++ src/main/java/com/example/ticstattoe/repository/GameRepository.java
@@ -2,8 +2,6 @@ package com.example.ticstattoe.repository;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 import com.example.ticstattoe.model.Game;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +9,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-  Optional<Game> findById(Long id);
 }
