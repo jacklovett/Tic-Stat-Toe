beb87c66f52b4a28417e7db6f080d5bd755ca84e
Jack Lovett <jack.l.lovett@gmail.com>
2020-10-16 00:22:35 +0300

    Add find methods to repos

--- src/main/java/com/example/ticstattoe/repository/BoardHistoryRepository.java
+++ src/main/java/com/example/ticstattoe/repository/BoardHistoryRepository.java
@@ -1,5 +1,7 @@
 package com.example.ticstattoe.repository;
 
+import java.util.Optional;
+
 import com.example.ticstattoe.model.BoardHistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoardHistoryRepository extends JpaRepository<BoardHistory, Long> {
 
+  Optional<BoardHistory> findByGameId(Long gameId);
 }
--- src/main/java/com/example/ticstattoe/repository/GameRepository.java
+++ src/main/java/com/example/ticstattoe/repository/GameRepository.java
@@ -2,6 +2,8 @@ package com.example.ticstattoe.repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import com.example.ticstattoe.model.Game;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+  Optional<Game> findById(Long id);
 }
