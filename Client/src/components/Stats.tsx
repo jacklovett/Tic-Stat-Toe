960f1609b390680c3a732f0c1d56141acfaa89b3
Jack Lovett <jack.l.lovett@gmail.com>
2020-10-21 20:49:57 +0300

    Remove duplicate Client directory

--- Client/src/components/Stats.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Text } from 'office-ui-fabric-react'
-
-export const Stats = () => {
-  return <Text>Hello from Stats</Text>
-}
--- Client/src/services/httpService.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-import { GameData } from '../types'
-
-export const saveGameData = (gameData: GameData) => {
-  axios.post('/api/game', gameData).catch((error) => {
-    console.error(error)
-  })
-}
