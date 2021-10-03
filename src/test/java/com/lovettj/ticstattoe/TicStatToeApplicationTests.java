cd41235c83b068ead8d19b78e69418b35087d5b2
Jack Lovett <jack.l.lovett@gmail.com>
2021-10-03 13:41:53 +0300

    Fix Tests attempt

--- src/test/java/com/lovettj/ticstattoe/TicStatToeApplicationTests.java
+++ src/test/java/com/lovettj/ticstattoe/TicStatToeApplicationTests.java
@@ -1,5 +1,7 @@
 package com.lovettj.ticstattoe;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class TicStatToeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
