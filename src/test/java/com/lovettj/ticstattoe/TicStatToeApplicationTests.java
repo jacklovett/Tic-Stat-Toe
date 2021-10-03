b63fbeb58b56085e45d5ec9b4e64c43ffb1c0c90
Jack Lovett <jack.l.lovett@gmail.com>
2021-10-03 14:02:43 +0300

    Fix Codacy warning and try and fix scripts

--- src/test/java/com/lovettj/ticstattoe/TicStatToeApplicationTests.java
+++ src/test/java/com/lovettj/ticstattoe/TicStatToeApplicationTests.java
@@ -1,7 +1,5 @@
 package com.lovettj.ticstattoe;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +8,7 @@ class TicStatToeApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
+
 	}
 
 }
